58c436a5fc8187f0bf5b0ba708f16073a8d7a6a2
Johan <60303081+alpacaswillrule@users.noreply.github.com>
2022-04-09 01:06:50 -0400

    battle done, now move and oneiter

--- main.py
+++ main.py
@@ -17,23 +17,28 @@ def initialize_simulation(NumPokemon = 15, Area = 300): #returns a list of each 
         Pokemonlst.append(Pokemon(statsdataframe.iloc[0],(x[i],y[i])))
     return Pokemonlst
     
-def battle(Pokemonlst, index1, index2):
+def battle(Pokemonlst, index1, index2): #hasnt been debugged yet
     stat1 = Pokemonlst[index1].getstats()
-    stat2 = Pokemonlst[index2].getstats()
-    #also need to get type advantage
-    firstadv = typechart[stat1[1]].loc[stat2[1]]
-    secondadv = typechart[stat2[1]].loc[stat1[1]]
+    stat2 = Pokemonlst[index2].getstats() # gets pokemon stats
+    
+    firstadv = typechart.loc[stat1[1]].loc[stat2[1]] #looks up type advantage in typechart, returns number from .5 to 2
+    secondadv = typechart.loc[stat2[1]].loc[stat1[1]]
 
 
-    stren1 = np.random.normal(loc=1,scale=.8) * stat1[3] * stat1[2] * stat1[4] * stat1[5] * stat1[7] * firstadv
+    stren1 = np.random.normal(loc=1,scale=.8) * stat1[3] * stat1[2] * stat1[4] * stat1[5] * stat1[7] * firstadv #calculates their strenghts, adds randomness
     stren2 = np.random.normal(loc=1,scale=.8) * stat2[3] * stat2[2] * stat2[4] * stat2[5] * stat2[7] * secondadv
 
+    if stren1>stren2: #the battle! one of them is killed, maybe adjust in future so theres a chance that one survives
+        Pokemonlst[index2].kill()
+    else:
+        Pokemonlst[index1].kill()
 
 
+def move(Pokemonlst,Area):
+    pass
 
-
-
-
+def oneiter(Pokemonlst, NumPokemon, Area):
+    pass
 
 initialize_simulation()
         
