f9a149b455ddb3fd91b948aa18c495e6778531d3
Johan <60303081+alpacaswillrule@users.noreply.github.com>
2022-04-09 16:30:33 -0400

    fixedbug in reproduce

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def gencoords(pos,speed,Area):
     dir = random.randint(0,4) #randomly generate a direction
     x = np.random.normal(loc=speed/10,scale=.5) # randomly generate how much to move in that directin
     y = np.random.normal(loc=speed/10,scale=.5)
-    newpos = (Area,Area)
+    newpos = [Area,Area]
     if dir == 0: # 4 different directions
         newpos[0] = pos[0] + x
         if newpos[0] > Area or newpos[0] < 0:  #checks to see if out of bounds, changes direction if out of bounds
@@ -81,13 +81,15 @@ def gencoords(pos,speed,Area):
     
     if pos[1] > Area or pos[0] > Area or pos[1] < 0 or pos[0] < 0: # strict invariant
         raise Exception('position output out of bounds gencoords')
-    return newpos
+    return tuple(newpos)
 
 
 def move(Pokemonlst,Area):
     for pokemon in Pokemonlst:
         speed = pokemon.getstats()[7]
         curpos = pokemon.getpos()
+        print("curpos")
+        print(curpos)
         pokemon.newpos(gencoords(curpos,speed,Area)) #moves pokemon to new coordinates based on their speed and area constraints
     return Pokemonlst
 
@@ -102,9 +104,9 @@ def reproduce(Pokemonlst,index1,index2,Area):
     if Pokemonlst[index1].can_reproduce() == True and Pokemonlst[index2].can_reproduce() == True:
         Pokemonlst[index1].reproduce()
         Pokemonlst[index1].reproduce()
-        x = random.sample(range(0, Area))
-        y = random.sample(range(0, Area))
-        Pokemonlst.append(Pokemon(statsdataframe.iloc[0],(x,y)))
+        x = random.sample(range(0, Area),1)
+        y = random.sample(range(0, Area),1)
+        Pokemonlst.append(Pokemon(statsdataframe.iloc[0],(x[0],y[0])))
 
     return Pokemonlst
 
@@ -114,27 +116,36 @@ def oneiter(Pokemonlst, NumPokemon, Area,engagedist): #use pdist here, run the m
     np.fill_diagonal(distmatrix,float('inf'))
     conflicts = np.argwhere(distmatrix<engagedist) 
     conflicts = set(conflicts.flatten()) #duplicates removed
+    it = iter(conflicts)
+    conflicts = list(zip(it,it))
+    #print(conflicts)
     for conflict in conflicts:
         if Pokemonlst[conflict[0]].getname() ==  Pokemonlst[conflict[1]].getname():
             Pokemonlst = reproduce(Pokemonlst,conflict[0],conflict[1],Area)
+            print("a pokemon was born")
         else:
             Pokemonlst = battle(Pokemonlst,conflict[0],conflict[1])
+            print("a pokemon died")
     for pokemon in Pokemonlst:
         pokemon.oneround()
     Pokemonlst = move(Pokemonlst, Area)
 
-    #first we find closeby pokemon, then they fight or fuck
-    #then oneround for cooldowns
-    #them move
 
 def visualize(Pokemonlst,NumPokemon,Area):
-    pass
+    x = extractcoordlist(Pokemonlst)
+    plt.scatter(*zip(*x))
+    plt.show()
 ###parameters, can also adjust reproduce cap in pokemonclass.py
 NumPokemon = 15
 Area = 300
 engage_dist = 10
+iterations = 20
 Pokemonlst = initialize_simulation(NumPokemon,Area)
-oneiter(Pokemonlst,NumPokemon,Area,engage_dist)
+visualize(Pokemonlst,NumPokemon,Area)
+while iterations > 0:
+    oneiter(Pokemonlst,NumPokemon,Area,engage_dist)
+    iterations-=1
+visualize(Pokemonlst,NumPokemon,Area)
         
 
 
