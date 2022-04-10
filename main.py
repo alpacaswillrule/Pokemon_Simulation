021d276b8130435fca824b6ad667dc238090030f
Johan <60303081+alpacaswillrule@users.noreply.github.com>
2022-04-10 15:18:36 -0400

    bugfixed battle and oneiter

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementTree import tostring
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,12 +10,13 @@ import random
 statsdataframe = pd.read_csv('Pokemon.csv')
 typechart = pd.read_csv('Pokemon_Type_Chart.csv')
 
-def initialize_simulation(NumPokemon, Area): #returns a list of each pokemon object, a list of coordinates aswell corresponding to list of pokemon objects
+def initialize_simulation(NumPokemon,Numduplicates, Area): #returns a list of each pokemon object, a list of coordinates aswell corresponding to list of pokemon objects
     Pokemonlst = []
-    x = random.sample(range(0, Area), NumPokemon)
-    y = random.sample(range(0, Area), NumPokemon)
-    for i in range(NumPokemon):
-        Pokemonlst.append(Pokemon(statsdataframe.iloc[i],(x[i],y[i])))
+    x = random.sample(range(0, Area), NumPokemon*Numduplicates)
+    y = random.sample(range(0, Area), NumPokemon*Numduplicates)
+    for j in range(0,NumPokemon*Numduplicates,Numduplicates):
+        for i in range(Numduplicates):
+            Pokemonlst.append(Pokemon(statsdataframe.iloc[j],(x[j+i],y[j+i])))
     return Pokemonlst
 
 def typetoindex(stat2):
@@ -72,11 +74,12 @@ def battle(Pokemonlst, index1, index2): #hasnt been debugged yet, also may be co
 
     if stren1>stren2: #the battle! one of them is killed, maybe adjust in future so theres a chance that one survives
         Pokemonlst[index2].kill()
-        Pokemonlst.pop(index2)
-
+        #Pokemonlst.pop(index2) #is now done later because was confounding loop through conflicts
+        print(Pokemonlst[index2].getname()+" has died")
     else:
         Pokemonlst[index1].kill()
-        Pokemonlst.pop(index1)
+        #Pokemonlst.pop(index1)
+        print(Pokemonlst[index1].getname()+" has died")
     return Pokemonlst
 
 def gencoords(pos,speed,Area):
@@ -151,41 +154,49 @@ def reproduce(Pokemonlst,index1,index2,Area):
 
     return Pokemonlst
 
-def oneiter(Pokemonlst, NumPokemon, Area,engagedist): #use pdist here, run the move function, and run their oneround functions then call battle function for one that encounter each other
+def oneiter(Pokemonlst, Area,engagedist): #use pdist here, run the move function, and run their oneround functions then call battle function for one that encounter each other
     dists = extractcoordlist(Pokemonlst)
     distmatrix = squareform(pdist(dists))
-    np.fill_diagonal(distmatrix,float('inf'))
-    conflicts = np.argwhere(distmatrix<engagedist) 
-    conflicts = set(conflicts.flatten()) #duplicates removed
+    np.fill_diagonal(distmatrix,float('inf')) #create distance matrix between all pokemon
+    conflicts = np.argwhere(distmatrix<engagedist)  # where pokemon are closer than engeagedist, there is a conflict
+    conflicts = set(conflicts.flatten()) #duplicates removed from conflicts
     it = iter(conflicts)
     conflicts = list(zip(it,it))
-    #print(conflicts)
-    for conflict in conflicts:
+    print(conflicts)
+    for conflict in conflicts: # problem, pokemonlst gets shortened by each battle so conflict indexes change, will change their status to is dead, then after for loop remove all isdead from list
+        print(conflict)
         if Pokemonlst[conflict[0]].getname() ==  Pokemonlst[conflict[1]].getname():
             Pokemonlst = reproduce(Pokemonlst,conflict[0],conflict[1],Area)
-            print("a pokemon was born")
+            print(conflict[0].getname()+" was born")
         else:
             Pokemonlst = battle(Pokemonlst,conflict[0],conflict[1])
-            print("a pokemon died")
+    
     for pokemon in Pokemonlst:
-        pokemon.oneround()
+        if pokemon.isalive() == False:
+            Pokemonlst.remove(pokemon)
+        else:
+            pokemon.oneround()
     Pokemonlst = move(Pokemonlst, Area)
 
 
-def visualize(Pokemonlst,NumPokemon,Area):
+def visualize(Pokemonlst,Area):
     x = extractcoordlist(Pokemonlst)
     plt.scatter(*zip(*x))
     plt.show()
+
+
+
 ###parameters, can also adjust reproduce cap in pokemonclass.py
 NumPokemon = 15
+Numduplicates = 2 #number of duplicates made of each pokemon
 Area = 300
 engage_dist = 10
 iterations = 20
-Pokemonlst = initialize_simulation(NumPokemon,Area)
+Pokemonlst = initialize_simulation(NumPokemon,Numduplicates,Area)
 
-visualize(Pokemonlst,NumPokemon,Area)
+visualize(Pokemonlst,Area)
 while iterations > 0:
-    oneiter(Pokemonlst,NumPokemon,Area,engage_dist)
+    oneiter(Pokemonlst,Area,engage_dist)
     iterations-=1
 visualize(Pokemonlst,NumPokemon,Area)
         
